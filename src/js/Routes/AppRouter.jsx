692edcb4dd2b5e8692b3b5321cabc68fb6e55d45
Mauricio Montoya <mmontoyaabadia@gmail.com>
2022-08-23 08:36:47 -0500

    Configurado para servidor local

--- src/js/Routes/AppRouter.jsx
+++ src/js/Routes/AppRouter.jsx
@@ -6,7 +6,7 @@ import { Home } from '../pages/Home';
 
 export const AppRouter = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/detalles/:id' element={<Detalles />}>
